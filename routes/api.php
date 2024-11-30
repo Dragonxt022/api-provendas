ed24684f14825a56b344104aa01c28fb5d846f0f
Dragonxt022 <pissinatti2019@gmail.com>
2024-11-30 14:01:01 -0400

    Implementando sistema de autentificação

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,23 @@
 use App\Http\Controllers\api\v1\EmpresaController;
 use App\Http\Controllers\api\v1\ProductController;
 use App\Http\Controllers\api\v1\UserController;
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\api\v1\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 // Sistema de login
+
+
+
 // Route::post('/register', [AuthController::class, 'register']);
-// Route::post('/login', [AuthController::class, 'login']);
+
 // Route::middleware(['auth:sanctum'])->post('/logout', [AuthController::class, 'logout']);
 
 // Grupo de Rotas da api v1
 Route::prefix('v1')->group(function () {
+
+    // Sistema de Autentificação
+    Route::post('/login', [AuthController::class, 'login']);
 
     // Usuarios
     Route::get('/users', [UserController::class, 'index']);
